b0b0b3a62d419a1377e11ac69c102051eafe4052
Payakorn Saksuriya <payakornn@gmail.com>
2022-05-07 14:01:41 +0700

    add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using VehicleRoutingSynPre
 using Test
 
+particle = generate_particles("ins10-1");
+particle.route, particle.slot = example();
+particle.starttime = find_starttime(particle)
+
 @testset "VehicleRoutingSynPre.jl" begin
     # Write your tests here.
+    @test objective_value(particle) - 218.19855033333334 < 1e-3
 end
